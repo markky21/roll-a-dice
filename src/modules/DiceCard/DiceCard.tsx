8cf122b892b7ace69fd3145fbdf7db64b75fcf2a
Marcin Mirecki <markky21@gmail.com>
2020-04-15 11:40:34 +0200

    Fix bug with shift dice faces in dice lib, created DiceService.context.tsx

--- src/modules/DiceCard/DiceCard.tsx
+++ src/modules/DiceCard/DiceCard.tsx
@@ -1,13 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
 import { Paper } from '@material-ui/core';
-import { useFirestore } from 'react-redux-firebase';
-import { useSelector } from 'react-redux';
 
 import { diceDefaultConfig } from '../../config/dice.config';
-import { DiceService } from '../../services/dice.service';
-import { firebaseSelectors } from '../../store/firebase/firebase.selectors';
-import { roomsSelectors } from '../../store/rooms/rooms.selectors';
+import { DiceServiceContext } from '../../contexts/DiceService.context';
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -31,25 +27,11 @@ function DiceCardC(props: DiceCardProps) {
   const diceContainerEl = useRef(null);
 
   const [diceInitialized, setDiceInitialized] = useState(false);
-  const firestore = useFirestore();
-  const profile = useSelector(firebaseSelectors.userProfile);
-  const roomUid = useSelector(roomsSelectors.selectedRoomUid);
-
-  const diceService = DiceService.getInstance(firestore);
+  const diceService = useContext(DiceServiceContext);
   const canvasWidth: number = (canvasRef.current && (canvasRef.current as any).getBoundingClientRect().width) || 0;
 
   useEffect(() => {
-    diceService.profile = profile;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [profile]);
-
-  useEffect(() => {
-    diceService.roomUid = roomUid || null;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [roomUid]);
-
-  useEffect(() => {
-    if (!diceInitialized && canvasWidth !== 0) {
+    if (!!diceService && !diceInitialized && canvasWidth !== 0) {
       (window as any).dice_initialize(diceContainerEl.current, {
         ...diceDefaultConfig,
         diceThrow$: diceService.diceThrow$,
@@ -60,10 +42,7 @@ function DiceCardC(props: DiceCardProps) {
       setDiceInitialized(true);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [canvasWidth]);
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => () => diceService.hostDestroyed(), []);
+  }, [!!canvasWidth, !!diceService]);
 
   return (
     <Paper elevation={3} className={classes.root}>
